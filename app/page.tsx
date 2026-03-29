74fb49e358e1ded15ecfaea5a7e6e84f61275543
70148811-pixel <70148811@student.uol.edu.pk>
2026-03-30 02:34:14 +0500

    Rename Software Reviews to Comparisons category

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function HomePage() {
       <HeroSection />
       
       {/* Category Feeds representing an authoritative B2B Resource Hub */}
-      <CategoryArticlesBlock categorySlug="software-reviews" categoryName="Software Reviews" />
+      <CategoryArticlesBlock categorySlug="comparisons" categoryName="Comparisons" />
       <CategoryArticlesBlock categorySlug="accounting" categoryName="Accounting" />
       <CategoryArticlesBlock categorySlug="payroll" categoryName="Payroll & HR" />
       <CategoryArticlesBlock categorySlug="getting-paid" categoryName="Getting Paid" />
